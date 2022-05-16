1b398f321366dab25d0557079a386ce1c5d6ea0f
Cleiton <cleiton.biou@gmail.com>
2022-05-16 11:06:50 -0300

    build
    Signed-off-by: Cleiton <cleiton.biou@gmail.com>

--- src/shared/infra/typeorm/migrations/1652113012446-CreateArticle.ts
+++ src/shared/infra/typeorm/migrations/1652113012446-CreateArticle.ts
@@ -3,67 +3,69 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateArticle1652113012446 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: 'articles',
-      columns: [
-        {
-          name: 'id',
-          type: 'integer',
-          isGenerated: true,
-          generatedIdentity: 'ALWAYS',
-          isPrimary: true,
-        },
-        {
-          name: 'featured',
-          type: 'boolean',
-          isNullable: true
-        },
-        {
-          name: 'title',
-          type: 'varchar',
-          isNullable: true
-        },
-        {
-          name: 'url',
-          type: 'varchar',
-          isNullable: true
-        },
-        {
-          name: 'imageUrl',
-          type: 'varchar',
-          isNullable: true
-        },
-        {
-          name: 'newsSite',
-          type: 'varchar',
-          isNullable: true
-        },
-        {
-          name: 'summary',
-          type: 'varchar',
-          isNullable: true
-        },
-        {
-          name: 'publishedAt',
-          type: 'timestamp',
-          isNullable: true
-        },
-        {
-          name: 'launches',
-          type: 'text',
-          isNullable: true
-        },
-        {
-          name: 'events',
-          type: 'text',
-          isNullable: true
-        }
-      ]
-    }))
+    queryRunner.createDatabase('articles').then(async () => {
+      await queryRunner.createTable(new Table({
+        name: 'articles',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isGenerated: true,
+            generatedIdentity: 'ALWAYS',
+            isPrimary: true,
+          },
+          {
+            name: 'featured',
+            type: 'boolean',
+            isNullable: true
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'url',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'imageUrl',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'newsSite',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'summary',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'publishedAt',
+            type: 'timestamp',
+            isNullable: true
+          },
+          {
+            name: 'launches',
+            type: 'text',
+            isNullable: true
+          },
+          {
+            name: 'events',
+            type: 'text',
+            isNullable: true
+          }
+        ]
+      }))
+    })
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropDatabase('articles')
     await queryRunner.dropTable('articles');
   }
-
 }
